a216b3b502e2ccbb144c529cce61ef39cdd030ca
React1 <React1@flyweis.technology>
2023-05-08 18:22:52 +0530

    Admin Panel

---
+++ .history/src/components/vendorPanel/components/pages/VendorDashboard_20230508180227.jsx
@@ -0,0 +1,102 @@
+import React from "react";
+import HOC from "../../../vendorPanel/components/layout/HOC";
+import { FiUser } from "react-icons/fi";
+import { useNavigate } from "react-router-dom";
+
+export const dash = (data) => {
+  console.log(data, "dsjkfhjkashfjk");
+  return data;
+};
+const VendorDashboard = () => {
+  const card = [
+    {
+      progress: "bg-blue-400",
+      title: "Total Admin",
+      number: "",
+      icon: (
+        <i className="fa-sharp fa-solid fa-money-bill text-2xl text-[rgb(240,72,88)]"></i>
+      ),
+      bg: "#ff5370",
+      link: "/dashboard",
+    },
+    {
+      progress: "bg-green-400",
+      title: "Total Users",
+      number: "",
+      icon: (
+        <i
+          className="fa-solid fa-user text-2xl"
+          style={{ color: "#4099ff" }}
+        ></i>
+      ),
+      bg: "#4099ff",
+      link: "/Customer",
+    },
+    {
+      progress: "bg-green-400",
+      title: "All Admin",
+      number: "",
+      icon: <FiUser className="text-2xl text-[#29cccc]" />,
+      bg: "#29cccc",
+      link: "/Admin",
+    },
+    {
+      progress: "bg-green-400",
+      title: "All Product",
+      number: "",
+      icon: <i class="fa-solid fa-cart-shopping text-2xl text-[#3c335d]"></i>,
+      bg: "#3c335d",
+      link: "/Product",
+    },
+    {
+      progress: "bg-green-400",
+      title: "All category",
+      number: "",
+      icon: <i className=" fa-brands fa-slack text-2xl text-[#64878e]"></i>,
+      bg: "#64878e",
+      link: "/Category",
+    }
+  ];
+  return (
+    <>
+      <section className="grid md:grid-cols-3 grid-cols-2 gap-y-6 gap-x-4">
+        {/* Card */}
+        {card.map((card , index) => {
+          return (
+            <div
+              className="px-5 py-8 bg-slate-200 space-y-2 shadow-xl flex flex-col  rounded-md cardDiv"
+              key={index}
+              style={{
+                backgroundColor: `${card.bg}`,
+                textTransform: "uppercase",
+              }}
+              onClick={() => navigate(`${card.link}`)}
+            >
+              <div className="grid  justify-between grid-cols-4">
+                <div className="flex flex-col col-span-3 space-y-1">
+                  <span
+                    className="tracking-widest text-gray-900"
+                    style={{ color: "#fff" }}
+                  >
+                    {card.title}
+                  </span>
+                  <span
+                    className="tracking-wider text-gray-700 text-xl md:text-2xl font-semibold"
+                    style={{ color: "#fff" }}
+                  >
+                    {card.number}
+                  </span>
+                </div>
+                <div className="flex rounded-full w-10 h-10 sm:w-12 sm:h-12 bg-white justify-center items-center iCOn">
+                  {card.icon}
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </section>
+    </>
+  );
+};
+
+export default HOC(VendorDashboard);
---
+++ .history/src/components/vendorPanel/components/pages/VendorDashboard_20230508180653.jsx
@@ -0,0 +1,110 @@
+import React, { useCallback, useEffect, useState } from "react";
+import HOC from "../../../vendorPanel/components/layout/HOC";
+import { FiUser } from "react-icons/fi";
+import { useNavigate } from "react-router-dom";
+
+const VendorDashboard = () => {
+  const navigate = useNavigate();
+  const token = localStorage.getItem("token");
+
+  const [ userCount , setUserCount ] = useState('')
+
+  const fetchData = useCallback(async () => {
+    try {
+      const { data } = await axios.get(
+        "https://6nkuu0ytgg.execute-api.ap-south-1.amazonaws.com/dev//api/v1/admin/users",
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      console.log(data)
+    } catch (err) {
+      console.log(err);
+    }
+  }, [token]);
+
+  useEffect(()
+
+  const card = [
+    {
+      progress: "bg-green-400",
+      title: "Total Users",
+      number: "",
+      icon: (
+        <i
+          className="fa-solid fa-user text-2xl"
+          style={{ color: "#4099ff" }}
+        ></i>
+      ),
+      bg: "#4099ff",
+      link: "/users",
+    },
+    {
+      progress: "bg-green-400",
+      title: "All Category",
+      number: "",
+      icon: <FiUser className="text-2xl text-[#29cccc]" />,
+      bg: "#29cccc",
+      link: "/category",
+    },
+    {
+      progress: "bg-green-400",
+      title: "All Product",
+      number: "",
+      icon: <i class="fa-solid fa-cart-shopping text-2xl text-[#3c335d]"></i>,
+      bg: "#3c335d",
+      link: "/product",
+    },
+    {
+      progress: "bg-green-400",
+      title: "All Coupon",
+      number: "",
+      icon: <i className=" fa-brands fa-slack text-2xl text-[#64878e]"></i>,
+      bg: "#64878e",
+      link: "/dis",
+    }
+  ];
+  return (
+    <>
+     <section className="grid md:grid-cols-4 grid-cols-2 gap-y-6 gap-x-4">
+        {card.map((card , index) => {
+          return (
+            <div
+              className="px-5 py-8 bg-slate-200 space-y-2 shadow-xl flex flex-col  rounded-md cardDiv"
+              key={index}
+              style={{
+                backgroundColor: `${card.bg}`,
+                textTransform: "uppercase",
+              }}
+              onClick={() => navigate(`${card.link}`)}
+            >
+              <div className="grid  justify-between grid-cols-4">
+                <div className="flex flex-col col-span-3 space-y-1">
+                  <span
+                    className="tracking-widest text-gray-900"
+                    style={{ color: "#fff" }}
+                  >
+                    {card.title}
+                  </span>
+                  <span
+                    className="tracking-wider text-gray-700 text-xl md:text-2xl font-semibold"
+                    style={{ color: "#fff" }}
+                  >
+                    {card.number}
+                  </span>
+                </div>
+                <div className="flex rounded-full w-10 h-10 sm:w-12 sm:h-12 bg-white justify-center items-center iCOn">
+                  {card.icon}
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </section>
+    </>
+  );
+};
+
+export default HOC(VendorDashboard);
